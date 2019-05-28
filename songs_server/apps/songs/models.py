55c265dacf010a6f00f97dad16723913a2148dfd
Cameron Wilkes <cwilkes17@gmail.com>
2019-05-28 14:32:09 -0700

    added validation to the songs model

--- songs_server/apps/songs/models.py
+++ songs_server/apps/songs/models.py
@@ -8,14 +8,24 @@ email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
 class SongManager(models.Manager):
-    pass
+    def addSongValidation(self, postData):
+        errors = {}
+
+        if len(postData['title']) < 2:
+            errors['title']= "Title must be at least 2 characters long"
+        if len(postData['artist']) < 2:
+            errors['artist'] = "Artist name nust be at least 2 characters long"
+        return errors
 
 
 class Song(models.Model):
-    artist = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
     user = models.ForeignKey(User, related_name = "songs")
     numAdded = models.ManyToManyField(User, related_name = "users")
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+
+
 

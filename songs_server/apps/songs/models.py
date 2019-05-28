6b40378dcc4cee08cf2e04a7c5a68c3a1e075cea
Cameron Wilkes <cwilkes17@gmail.com>
2019-05-28 13:36:26 -0700

    added songs model/class

--- songs_server/apps/songs/models.py
+++ songs_server/apps/songs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from ..users.models import User
+import re
+import bcrypt
 
-# Create your models here.
+
+email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+
+class SongManager(models.Manager):
+    pass
+
+
+class Song(models.Model):
+    artist = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, related_name = "songs")
+    numAdded = models.ManyToManyField(User, related_name = "users")
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
